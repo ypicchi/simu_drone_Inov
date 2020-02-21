69474b06809ced371108fab83e0db6a056f45bae
ypicchi <yoan.picchi@etu.u-bordeaux.fr>
2020-02-21 12:10:16 +0100

    commit initial du drone fille

---
+++ simuRechercheDVA/Assets/Scripts/ChildNavigation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildNavigation : Navigation
+{
+    //TODO child Navigation
+    protected override void GenerateNextNavigationWaypoint(){
+
+    }
+	
+	protected override List<Vector3> computeTargetsPositions(){
+        //TODO
+		List<Vector3> targets = new List<Vector3>();
+		targets.Add(sensor.GetPosition());
+        return targets;
+    }
+    
+}
